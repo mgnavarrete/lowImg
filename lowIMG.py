206fe758e7f06e5120c92ef8da0820834bda0a1d
mgnavarrete <mgnavarrete@uc.cl>
2024-06-18 11:09:21 -0400

    run on win fix

--- lowIMG.py
+++ lowIMG.py
@@ -13,14 +13,12 @@ import shutil
 parser = argparse.ArgumentParser(description='Ejecutar script con un argumento de línea de comandos')
 
 # Agrega un argumento posicional para el task_name
-parser.add_argument('proceso', type=str, help='Nombre del proceso, opciones: new_task o get_data')
 parser.add_argument('task_name', type=str, help='Nombre de la tarea, formato: levID-medID')
 
 # Parsea los argumentos proporcionados por la línea de comandos
 args = parser.parse_args()
 
 # Usa el argumento task_name en tu programa
-proceso = args.proceso
 task_name = args.task_name
 
 # Carga las variables del archivo .env
@@ -37,59 +35,57 @@ CVAT_PASSWORD = os.getenv('CVAT_PASSWORD')
 
 # Conectar a CVAT
 client = connect_to_cvat(CVAT_HOST, CVAT_USERNAME, CVAT_PASSWORD)
-
-if proceso == 'get_img':
     
-    # Buscar la tarea
-    task = find_task(client, task_name)
+# Buscar la tarea
+task = find_task(client, task_name)
 
-    # Obtener el dataset con lables e imagenes
-    get_dataset(task, CVAT_HOST, CVAT_USERNAME, CVAT_PASSWORD)
+# Obtener el dataset con lables e imagenes
+get_dataset(task, CVAT_HOST, CVAT_USERNAME, CVAT_PASSWORD)
 
-    # Correr unzip para descomprimir el archivo
-    print(f"Descomprimiendo el archivo {task_name}.zip...")
-    with zipfile.ZipFile(f'{task_name}.zip', 'r') as zip_ref:
-        zip_ref.extractall(task_name)
-    
-    print("Archivo descomprimido!")
+# Correr unzip para descomprimir el archivo
+print(f"Descomprimiendo el archivo {task_name}.zip...")
+with zipfile.ZipFile(f'{task_name}.zip', 'r') as zip_ref:
+    zip_ref.extractall(task_name)
 
-    rootPath = os.path.join(task_name, 'obj_train_data', task_name.split('-')[0], task_name.split('-')[1], 'images')
-    lowImg = os.path.join(task_name, 'obj_train_data', task_name.split('-')[0], task_name.split('-')[1], 'img_mala_calidad')
+print("Archivo descomprimido!")
 
-    os.makedirs(lowImg, exist_ok=True)
-    images = os.listdir(rootPath)
+rootPath = os.path.join(task_name, 'obj_train_data', task_name.split('-')[0], task_name.split('-')[1], 'images')
+lowImg = os.path.join(task_name, 'obj_train_data', task_name.split('-')[0], task_name.split('-')[1], 'img_mala_calidad')
 
-    for filename in tqdm(images, desc="Bajando calidad IMG"):
-        if filename.endswith('.JPG'):
-            image_path = os.path.join(rootPath, filename)
-            imgData = cv2.imread(image_path)
-            imgResized = cv2.resize(imgData, (870, 650))
+os.makedirs(lowImg, exist_ok=True)
+images = os.listdir(rootPath)
 
-            cv2.imwrite(os.path.join(lowImg, filename), imgResized)
+for filename in tqdm(images, desc="Bajando calidad IMG"):
+    if filename.endswith('.JPG'):
+        image_path = os.path.join(rootPath, filename)
+        imgData = cv2.imread(image_path)
+        imgResized = cv2.resize(imgData, (870, 650))
 
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=AWS_DEFAULT_REGION
-    )
+        cv2.imwrite(os.path.join(lowImg, filename), imgResized)
 
-    # Recorrer todos los archivos en la carpeta local
-    for root, dirs, files in os.walk(lowImg):
-        for file in tqdm(files, desc="Subiendo archivos a S3"):
-            local_path = os.path.join(root, file)
-            s3_key = os.path.relpath(local_path, lowImg)
-            s3_full_key = os.path.join(task_name.split('-')[0], task_name.split('-')[1], 'img_mala_calidad', s3_key)
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_DEFAULT_REGION
+)
 
-            try:
-                s3.upload_file(local_path, AWS_BUCKET, s3_full_key)
-            except NoCredentialsError:
-                print("No se encontraron las credenciales para AWS.")
-            except Exception as e:
-                print(f"Error al subir el archivo {file}: {str(e)}")   
+# Recorrer todos los archivos en la carpeta local
+for root, dirs, files in os.walk(lowImg):
+    for file in tqdm(files, desc="Subiendo archivos a S3"):
+        local_path = f"{task_name}/obj_train_data/{task_name.split('-')[0]}/{task_name.split('-')[1]}/images/{file}"
+       
+        s3_full_key = f"{task_name.split('-')[0]}/{task_name.split('-')[1]}/img_mala_calidad/{file}"
 
-    # Eliminar el archivo zip y la carpeta descomprimida
-    shutil.rmtree(task_name)
-    os.remove(f'{task_name}.zip')
-    print("Archivos locales eliminados!")
-    print("Proceso finalizado!")
+        try:
+            s3.upload_file(local_path, AWS_BUCKET, s3_full_key)
+        except NoCredentialsError:
+            print("No se encontraron las credenciales para AWS.")
+        except Exception as e:
+            print(f"Error al subir el archivo {file}: {str(e)}")   
+
+# Eliminar el archivo zip y la carpeta descomprimida
+shutil.rmtree(task_name)
+os.remove(f'{task_name}.zip')
+print("Archivos locales eliminados!")
+print("Proceso finalizado!")
